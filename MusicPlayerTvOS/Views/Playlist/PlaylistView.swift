51ede50f5051f816b0cb38f2f92fe2b6646d8902
avijeet <avijeetpandey25@gmail.com>
2024-05-23 23:28:38 +0530

    Add: skeleton playlist view

---
+++ MusicPlayerTvOS/Views/Playlist/PlaylistView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaylistView.swift
+//  MusicPlayerTvOS
+//
+//  Created by Avijeet on 23/05/24.
+//
+
+import SwiftUI
+
+// MARK: - PlaylistView
+struct PlaylistView: View {
+    var body: some View {
+        Text("Playlist view")
+    }
+}
+
+
+#Preview {
+    PlaylistView()
+}
